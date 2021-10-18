61661dd5f7391d80a259fbc123e09e44380caffd
cidrblock <brad@thethorntons.net>
2021-10-18 09:51:19 -0700

    DOCs

--- plugins/modules/walk.py
+++ plugins/modules/walk.py
@@ -12,40 +12,48 @@ __metaclass__ = type
 DOCUMENTATION = r"""
 ---
 module: walk
-short_description: 
+short_description: Perform an SNMP walk against a remote device for one or more OIDs
 version_added: "1.0.0"
 description:
-  - 
+  - Perform an SNMP walk against a remote device for one or more OIDs
 options:
   best_guess:
-    description:
-      -
-    type: bool
-    default: False
+    description: 
+    - This setting controls how return value oids are parsed. 
+    - Setting to 0 causes a regular lookup.  
+    - Setting to 1 causes a regular expression match (defined as -Ib in snmpcmd). 
+    - Setting to 2 causes a random access lookup (defined as -IR in snmpcmd).
+    type: int
+    choices:
+    - 0
+    - 1
+    - 2
+    default: 0
   enums:
     description:
-      - 
+      - Set to `True` to have integer return values converted to enumeration identifiers if possible.
     type: bool
     default: True
   long_names:
     description:
-      - 
+      - Set to `True` to have OIDS generated preferring longer Mib name convention.
     type: bool
     default: False
   oids:
     description:
-      - 
+      - A list of OIDS to query on the remote device
     type: list
     entries: str
     required: True
   numeric:
     description:
-      - 
+      - Set to `True to have `oids` returned untranslated (i.e. dotted-decimal).
     type: bool
     default: False
   sprint_value:
     description:
-      -
+      - Set to `True` to have return values formatted with netsnmp's sprint_value function. 
+      - This will result in certain data types being returned in non-canonical format 
     type: bool
     default: True
 
